ed2e677f997a3a5bc3a73bf117734ff247c74bca
hnalpha323 <hnalpha323@gmail.com>
2015-05-09 23:03:01 +0500

    DB

---
+++ DataBAsee/Form1.cs
@@ -0,0 +1,87 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace DataBAsee
+{
+    public partial class Form1 : Form
+    {
+        private OleDbConnection connection = new OleDbConnection();
+
+        public Form1()
+        {
+            InitializeComponent();
+            connection.ConnectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=D:\db1.accdb;
+Persist Security Info=False;";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                connection.Open();
+
+                label4.Text = "Connection Successful";
+                connection.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error" + ex);
+            }
+        }
+
+
+       
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            connection.Open();
+            OleDbCommand command = new OleDbCommand();
+            command.Connection = connection;
+            command.CommandText = "select * from Table1 where Username ='" + textBox1.Text + "' and Password = '" + textBox2.Text + "'";
+
+            OleDbDataReader reader = command.ExecuteReader();
+            
+
+
+            int count = 0;
+            while (reader.Read())
+            {
+                count++;
+
+            }
+
+            if (count == 1)
+            {
+                MessageBox.Show("Username and Password are Correct");
+                connection.Close();
+                connection.Dispose();
+                this.Hide();
+                Form2 f2 = new Form2();
+
+
+
+                f2.ShowDialog();
+            }
+            else if (count > 1)
+            {
+                MessageBox.Show("Duplicate Username and Password");
+            }
+            else
+            {
+                MessageBox.Show("InCorrect Username and Password!");
+            }
+            connection.Close();
+        }
+
+        
+    }
+}
---
+++ DataBAsee/Form2.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace DataBAsee
+{
+    public partial class Form2 : Form
+    {
+        private OleDbConnection connection = new OleDbConnection();
+        public Form2()
+        {
+            InitializeComponent();
+            connection.ConnectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=D:\db1.accdb;
+Persist Security Info=False;";
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+        List<String> i = new List<String>();
+        List<String> g = new List<String>();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            connection.Open();
+            OleDbCommand command = new OleDbCommand("select * from Table1", connection);
+
+
+            OleDbDataReader reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                g.Add(reader["Roll_No"].ToString());
+                i.Add(reader["Grade"].ToString());
+
+            }
+            connection.Close();
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                int totalItems = g.Count;
+                int count = 0;
+                while (count < totalItems)
+                {
+                    if (txt_roll.Text == g[count].ToString())
+                    {
+                        listBox1.Items.Add(i[count].ToString());
+                        count = 100;
+                    }
+                    else
+                    {
+                        count++;
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error" + ex);
+            }
+
+        } 
+
+        private void txt_roll_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
